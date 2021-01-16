aad0144e5a835dc6d565c1c6329e05dca2b54cae
Krushil <krushilnaik96@gmail.com>
2021-01-16 17:22:09 -0500

    make indentation more like what I'm used to

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,7 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+	return render(request, "encyclopedia/index.html", {
+		"entries": util.list_entries()
+	})
 
