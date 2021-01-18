88e5803256c3f86d4796919bcf0eaa288a5ffc23
Krushil <krushilnaik96@gmail.com>
2021-01-18 14:36:00 -0500

    support various different "markdowns" in a single file line

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 import re
-# from typing import Match
+# from typing import Pattern, OrderedDict
+# from collections import OrderedDict
 from django.shortcuts import render
 
 from . import util
@@ -12,53 +13,83 @@ def index(request):
 
 
 def entry(request, title):
-	# def md2html(markdown):
-	# 	return
-
 	def createTag(tagType, content, **kwargs):
-		attributes = " ".join([f"{k}={v}" for k, v in kwargs.items()])
-		return f"<{tagType} {attributes if attributes else ''}>{content}</{tagType}>"
+		attributes = " ".join([f"{k}=\"{v}\"" for k, v in kwargs.items()])
+		return f"<{tagType}{' ' + attributes if attributes else ''}>{content}</{tagType}>"
 
-	linkRegex = re.compile(r"(\[.+?\]\(.+?\))")
-	italicRegex =     re.compile(r"( [*_]{1}[^*_]+?[*_]{1})")
-	boldRegex =       re.compile(r"( [*_]{2}[^*_]+?[*_]{2})")
+	anchorRegex     = re.compile(r"(\[.+?\]\(.+?\))")
+	italicRegex     = re.compile(r"( [*_]{1}[^*_]+?[*_]{1})")
+	boldRegex       = re.compile(r"( [*_]{2}[^*_]+?[*_]{2})")
 	boldItalicRegex = re.compile(r"( [*_]{3}[^*_]+?[*_]{3})")
-	listRegex = re.compile(r"([*-]{1} .+)")
-	hRegex = re.compile(r"^(#{1,6} .+)")
+	listItemRegex   = re.compile(r"^( *[*-]{1} .+)")
+	headerRegex     = re.compile(r"^(#{1,6} .+)")
 
-	regexList = {
-		"a": linkRegex,
-		"em strong": boldItalicRegex,
-		"strong": boldRegex,
-		"em": italicRegex,
-		"h": hRegex,
-		"li": listRegex,
-	}
+	regexList = [
+		anchorRegex,
+		boldItalicRegex,
+		boldRegex, italicRegex,
+		headerRegex, listItemRegex,
+	]
+
+	masterRegex = "|".join([regex.pattern for regex in regexList])
+	masterRegex = re.compile(masterRegex)
 
 	content = ""
 
-	previousListLevel = 0
+	previousListLevel = -1
 	with open(f"./entries/{title}.md", "r") as data:
 		for line in data:
-			if not line:
+			tagFound = False
+			if not line.rstrip():
 				continue
-			for tag, regex in regexList.items():
-				current = regex.split(line)
-				if len(current) > 1:
-					if tag == "a":
-						pass
-					elif tag == "li":
-						currentListLevel = len(current[1]) // 2
+			matches = masterRegex.split(line.rstrip())
+			for match in matches:
+				if not match:
+					continue
+				if headerRegex.match(match):
+					tagFound = True
+					cutoff = match.index("# ") + 1
+					content += createTag(f"h{cutoff}", match[cutoff+1:])
+				elif boldItalicRegex.match(match):
+					tagFound = True
+					content += createTag(
+						"strong",
+						createTag("em", match.strip()[3:-3])
+					)
+				elif boldRegex.match(match):
+					tagFound = True
+					content += createTag("strong", match.strip()[2:-2])
+				elif italicRegex.match(match):
+					tagFound = True
+					content += createTag("em", match.strip()[1:-1])
+				elif anchorRegex.match(match):
+					tagFound = True
+					text = match[1:match.index("]")]
+					link = match[match.index("(")+1:-1]
+					content += createTag("a", text, href=link)
+				elif listItemRegex.match(match):
+					tagFound = True
+					currentListLevel = 0
+					while match[currentListLevel] == " ":
+						currentListLevel += 1
+					currentListLevel //= 2
+					if currentListLevel > previousListLevel:
+						content += "<ul>"
+					elif currentListLevel < previousListLevel:
+						content += "</ul>"
+					content += createTag("li", match[currentListLevel*2+2:])
+					previousListLevel = currentListLevel
+				elif previousListLevel != -1:
+					for _ in range(previousListLevel + 1):
+						previousListLevel -= 1
+						content += "</ul>"
+				elif tagFound:
+					content += createTag("span", match)
 
-						if currentListLevel > previousListLevel:
-							previousListLevel += 1
-							content += "<ul>"
-						elif currentListLevel < previousListLevel:
-							content += "</ul>"*(previousListLevel - currentListLevel)
+			if not tagFound:
+				content += createTag("p", line.rstrip())
 
-						content += createTag("li", current[-2])
-					else:
-						pass
+	content = content.replace("><", ">\n<")
 
 	return render(request, "encyclopedia/entry.html", {
 		title: title,
