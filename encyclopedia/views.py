b226f7808b025ddd7cd22fd70414bb5aa55864e3
Krushil <krushilnaik96@gmail.com>
2021-01-18 19:25:53 -0500

    use provided util function to get .md content

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -33,58 +33,59 @@ def entry(request, title):
 
 	content = ""
 
-	previousListLevel = -1
-	with open(f"./entries/{title}.md", "r") as data:
-		for line in data:
-			tagFound = False
-			if not line.rstrip():
-				continue
-			matches = masterRegex.split(line.rstrip())
-			for match in matches:
-				if not match:
-					continue
-				if headerRegex.match(match):
-					tagFound = True
-					cutoff = match.index("# ") + 1
-					content += createTag(f"h{cutoff}", match[cutoff+1:])
-				elif boldItalicRegex.match(match):
-					tagFound = True
-					content += createTag(
-						"strong",
-						createTag("em", match.strip()[3:-3])
-					)
-				elif boldRegex.match(match):
-					tagFound = True
-					content += createTag("strong", match.strip()[2:-2])
-				elif italicRegex.match(match):
-					tagFound = True
-					content += createTag("em", match.strip()[1:-1])
-				elif anchorRegex.match(match):
-					tagFound = True
-					text = match[1:match.index("]")]
-					link = match[match.index("(")+1:-1]
-					content += createTag("a", text, href=link)
-				elif listItemRegex.match(match):
-					tagFound = True
-					currentListLevel = 0
-					while match[currentListLevel] == " ":
-						currentListLevel += 1
-					currentListLevel //= 2
-					if currentListLevel > previousListLevel:
-						content += "<ul>"
-					elif currentListLevel < previousListLevel:
-						content += "</ul>"
-					content += createTag("li", match[currentListLevel*2+2:])
-					previousListLevel = currentListLevel
-				elif previousListLevel != -1:
-					for _ in range(previousListLevel + 1):
-						previousListLevel -= 1
-						content += "</ul>"
-				elif tagFound:
-					content += createTag("span", match)
+	data = util.get_entry(title).split("\n")
 
-			if not tagFound:
-				content += createTag("p", line.rstrip())
+	previousListLevel = -1
+	for line in data:
+		tagFound = False
+		if not line.rstrip():
+			continue
+		matches = masterRegex.split(line.rstrip())
+		for match in matches:
+			if not match:
+				continue
+			if headerRegex.match(match):
+				tagFound = True
+				cutoff = match.index("# ") + 1
+				content += createTag(f"h{cutoff}", match[cutoff+1:])
+			elif boldItalicRegex.match(match):
+				tagFound = True
+				content += createTag(
+					"strong",
+					createTag("em", match.strip()[3:-3])
+				)
+			elif boldRegex.match(match):
+				tagFound = True
+				content += createTag("strong", match.strip()[2:-2])
+			elif italicRegex.match(match):
+				tagFound = True
+				content += createTag("em", match.strip()[1:-1])
+			elif anchorRegex.match(match):
+				tagFound = True
+				text = match[1:match.index("]")]
+				link = match[match.index("(")+1:-1]
+				content += createTag("a", text, href=link)
+			elif listItemRegex.match(match):
+				tagFound = True
+				currentListLevel = 0
+				while match[currentListLevel] == " ":
+					currentListLevel += 1
+				currentListLevel //= 2
+				if currentListLevel > previousListLevel:
+					content += "<ul>"
+				elif currentListLevel < previousListLevel:
+					content += "</ul>"
+				content += createTag("li", match[currentListLevel*2+2:])
+				previousListLevel = currentListLevel
+			elif previousListLevel != -1:
+				for _ in range(previousListLevel + 1):
+					previousListLevel -= 1
+					content += "</ul>"
+			elif tagFound:
+				content += createTag("span", match)
+
+		if not tagFound:
+			content += createTag("p", line.rstrip())
 
 	return render(request, "encyclopedia/entry.html", {
 		"title": title,
